6301d05b28b6f5f98af90fafbe124c81c5ed5881
Pascal Stammer <pstammer@agile-im.de>
2017-11-02 15:50:38 +0100

    first version of 1.2

---
+++ testat1/break_words.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+	int c;
+
+	while((c = getchar()) != EOF) {
+		if(c == ' ') {
+			putchar('\n');
+		} else {
+			putchar(c);
+		}
+	}
+
+	return 0;
+}
